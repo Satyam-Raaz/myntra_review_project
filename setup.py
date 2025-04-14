a84d06101f93e52ad0acec9bf25d0afb89444267
satyam <kumarsatyam1412@gmail.com>
2025-04-14 12:57:39 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='satyam',
+    author_email='kumarsatyam1412@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
